a3ebd5106a7c28a7aab88cbc0d4f137ff1cd6ec9
Jonatas Vitalino <jcv@cin.ufpe.br>
2020-01-17 02:15:11 -0300

    Classes criadas

---
+++ Entities/Order.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Pedido.Entities.Enums;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pedido.Entities
+{
+    class Order
+    {
+        DateTime Moment;
+        OrderStatus Status;
+        Client Client;
+        List<OrderItem> Items = new List<OrderItem>();
+
+        public Order() { }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+            double sum = 0.0;
+                foreach(OrderItem item in Items)
+            {
+                sum += item.SubTotal(item.Quantity,item.Price);
+
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Entities/OrderItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pedido.Entities
+{
+    class OrderItem
+    {
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Product Product { get; set; }
+
+        public OrderItem() { }
+
+        public OrderItem(int quantity, double price, Product product)
+        {
+            Quantity = quantity;
+            Price = price;
+            Product = product;
+        }
+
+        public double SubTotal(int quantity, double price)
+        {
+            return quantity * price;
+        }
+    }
+
+}
